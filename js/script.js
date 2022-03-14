f7f862843cf9d57e67277f7c74860619633fcef3
max-konokhov <makc.kohoxob@gmail.com>
2022-03-14 17:35:33 +0300

    добавил обратную анимацию

--- js/script.js
+++ js/script.js
@@ -15,21 +15,59 @@ let bgMenuBtn = document.querySelector('.burger');
 let bgMenuBtnClose = document.querySelector('.close');
 let bgMenu = document.querySelector('.menu');
 
-bgMenuBtn.addEventListener('click', function(){
-	bgMenu.classList.toggle('menu--open');
-  let openTimeLine = gsap.timeline();
 
-  openTimeLine.from('.menu', {opacity: 0, duration: 2})
-              .from('.menu__top', {opacity: 0, duration: 1, y: -140}, "-=1.5")
-              .from('.menu__container', {opacity: 0, duration: 2, y: 80}, "-=2")
-              .from('.nav__list', {opacity: 0, duration: 1, y: 50 }, "-=1")
-              .from('.social', {opacity: 0, duration: 1, y: 50 }, "-=0.5")
-              .from('.sub-menu', {opacity: 0, duration: 1, y: 50 }, "-=1")
-})
+// рабочий вариант без закрытия
+
+// bgMenuBtn.addEventListener('click', function(){
+// 	bgMenu.classList.toggle('menu--open');
+//   let openTimeLine = gsap.timeline();
+
+
+//   openTimeLine.from('.menu', {opacity: 0, duration: 2})
+//               .from('.menu__top', {opacity: 0, duration: 1, y: -140}, "-=1.5")
+//               .from('.menu__container', {opacity: 0, duration: 2, y: 80}, "-=2")
+//               .from('.nav__list', {opacity: 0, duration: 1, y: 50 }, "-=1")
+//               .from('.social', {opacity: 0, duration: 1, y: 50 }, "-=0.5")
+//               .from('.sub-menu', {opacity: 0, duration: 1, y: 50 }, "-=1")
+// })
+
+// bgMenuBtnClose.addEventListener('click', function(){
+//   bgMenu.classList.toggle('menu--open');
+// })
+
+
+
+// вариант 2 отрабатывает открытие и закрытие 1 раз.
+
+let openTimeLine2 = gsap.timeline();
+
+bgMenuBtn.addEventListener('click', function(){
+  bgMenu.classList.add('menu--open');
+
+  openTimeLine2
+    .from('.menu', {opacity: 0, duration: 2})
+    .from('.menu__top', {opacity: 0, duration: 1, y: -140}, "-=1.5")
+    .from('.menu__container', {opacity: 0, duration: 2, y: 80}, "-=2")
+    .from('.nav__list', {opacity: 0, duration: 1, y: 50 }, "-=1")
+    .from('.social', {opacity: 0, duration: 1, y: 50 }, "-=0.5")
+    .from('.sub-menu', {opacity: 0, duration: 1, y: 50 }, "-=1")
+});
+
 
 bgMenuBtnClose.addEventListener('click', function(){
-	bgMenu.classList.toggle('menu--open');
+  setTimeout(() => bgMenu.classList.remove('menu--open'), 4000);
+  openTimeLine2.reverse();
+
 })
+
+
+// bgMenuBtnClose.onclick = function() {
+//   timeline.reverse()
+// }
+
+
+
+
 
 
 
